67d849d070995eb8b1367ec0f05046bba9ee9ebd
Mark Novak <mark@m8it.io>
2023-11-01 17:43:17 +0100

    feat: add fluent validation

--- Services/DimensionsService.cs
+++ Services/DimensionsService.cs
@@ -2,13 +2,13 @@ namespace CompanionApp.Services;
 
 public interface IDimensionsService
 {
-    double CalculateGap(double dimension, double marginLeft, double marginRight, int pointsCount);
+    double CalculateGap(int dimension, int marginLeft, int marginRight, int pointsCount);
 }
 
 public class DimensionsService : IDimensionsService
 {
-    public double CalculateGap(double dimension, double marginLeft, double marginRight, int pointsCount) =>
+    public double CalculateGap(int dimension, int marginLeft, int marginRight, int pointsCount) =>
         pointsCount != 1
-            ? (dimension - (marginLeft + marginRight)) / (pointsCount - 1)
-            : (dimension - (marginLeft + marginRight)) / 2; // sets the point right in the middle 
+            ? (double)(dimension - (marginLeft + marginRight)) / (pointsCount - 1)
+            : (double)(dimension - (marginLeft + marginRight)) / 2; // sets the point right in the middle 
 }
--- Services/DimensionsStore.cs
+++ Services/DimensionsStore.cs
@@ -8,10 +8,15 @@ public class DimensionsStore
     public double MarginLeft { get; set; }
     public double MarginRight { get; set; }
     public int PointsCount { get; set; }
-    public double Gap => _dimensionsService.CalculateGap(Dimension, MarginLeft, MarginRight, PointsCount);
+    public double Gap { get; private set; }
 
     public DimensionsStore(IDimensionsService dimensionsService)
     {
         _dimensionsService = dimensionsService;
     }
+
+    // public void Calculate()
+    // {
+    //     Gap = _dimensionsService.CalculateGap(Dimension, MarginLeft, MarginRight, PointsCount);
+    // }
 }
